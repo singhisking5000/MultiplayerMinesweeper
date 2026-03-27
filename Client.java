bda3e8ec2fc4eb6d04c8f9c91ddf009ba224af8c
singhisking5000 <73306374+singhisking5000@users.noreply.github.com>
2026-03-27 16:13:50 +0000

    some gui stuff

--- Client.java
+++ Client.java
@@ -3,16 +3,36 @@ import javax.swing.*;
 
 public class Client
 {
+    private static JFrame f;
+    private static JLabel top;
+    private static JLabel bottom;
+    private static JPanel board;
+
+    private static int a;
+    private static int b;
+
+
+
     public static void main(String[] args)
     {
-
+        setupGUI();
     }
 
 
 
-    private void setupGUI()
+    private static void setupGUI()
     {
-
+        f = new JFrame();
+        top = new JLabel();
+        bottom = new JLabel();
+        board = new JPanel();
+        a = 50;
+        b = 50;
+        f.setLayout(new BorderLayout());
+        board.setLayout(new GridLayout(a, b));
+        f.add(top, BorderLayout.NORTH);
+        f.add(board, BorderLayout.CENTER);
+        f.add(bottom, BorderLayout.SOUTH);
     }
 
     private void updateGUI(GamePacket info)
